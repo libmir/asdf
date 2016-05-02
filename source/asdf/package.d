e373ba8df3dc5f2377d6de4c66c83089a64f1295
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-05-02 22:25:29 +0200

    fix unites for package

--- source/asdf/package.d
+++ source/asdf/package.d
@@ -17,29 +17,30 @@ T4=$(TR $(TDNW $(LREF $1)) $(TD $2) $(TD $3) $(TD $4))
 +/
 module asdf;
 
+///
+unittest
+{
+	void filterByLine()
+	{
+		import std.conv;
+		import std.stdio;
+		import std.format;
+		auto values = File("lines.jsonl").byChunk(4096).parseJsonByLine(4096);
+		size_t len, count;
+		FormatSpec!char fmt;
+		auto wr = stdout.lockingTextWriter;
+		foreach(val; values)
+		{
+			len += val.data.length;
+			if(val.getValue(["key"]) == "value")
+			{
+				count++;
+				wr.formatValue(val, fmt);
+				wr.put("\n");
+			}
+		}
+	}
+}
+
 public import asdf.asdf;
 public import asdf.jsonparser;
-
-
-
-//unittest
-//{
-//	import std.datetime;
-//	import std.conv;
-//	import std.stdio;
-//	import std.format;
-//	auto values = File("lines.jsonl").byChunk(4096).parseAsdfByLine(4096);
-//	size_t len, count;
-//	FormatSpec!char fmt;
-//	auto wr = stdout.lockingTextWriter;
-//	foreach(val; values)
-//	{
-//		len += val.data.length;
-//		if(val.getValue(["dsrc"]) == "20min.ch")
-//		{
-//			count++;
-//			wr.formatValue(val, fmt);
-//			wr.put("\n");
-//		}
-//	}
-//}
