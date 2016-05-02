26a4877523db3e5833f7efaa91c6b56e4d2b381a
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-05-02 22:22:18 +0200

    remove unittest from package

--- source/asdf/package.d
+++ source/asdf/package.d
@@ -21,20 +21,6 @@ public import asdf.asdf;
 public import asdf.jsonparser;
 
 
-unittest
-{
-	import std.conv: to;
-	import std.range;
-	auto text = cast(const ubyte[])`{"a":[true],"b":false,"c":32323,"dsdsd":{"a":true,"b":false,"c":"32323","d":null,"dsdsd":{}}}`;
-	auto asdf = text.chunks(13).parseJson(32);
-	import std.stdio;
-	assert(asdf.getValue(["dsdsd", "d"]) == null);
-	assert(asdf.getValue(["dsdsd", "a"]) == true);
-	assert(asdf.getValue(["dsdsd", "b"]) == false);
-	assert(asdf.getValue(["dsdsd", "c"]) == "32323");
-	assert(asdf.to!string == text);
-}
-
 
 //unittest
 //{
