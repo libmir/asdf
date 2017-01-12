9e61f3923ac898bea3b210097bdac05d53458d19
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2017-01-12 17:46:59 +0200

    add JSONTestSuite

---
+++ test_parser.sh
@@ -0,0 +1,9 @@
+dub build
+if [ -d JSONTestSuite ]; then
+	echo "JSONTestSuite already exist"
+else
+	git clone https://github.com/nst/JSONTestSuite
+fi
+cp run_tests.py JSONTestSuite/run_tests.py
+cd JSONTestSuite
+python3 run_tests.py
