4d6c6da0f17e05924430b5d5ea8900c4d4dc34c0
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2017-07-31 17:26:06 +0700

    update sajson benchmark

--- benchmarks/sajson/source/app.d
+++ benchmarks/sajson/source/app.d
@@ -15,7 +15,7 @@ immutable files =
     "instruments.json",
     "mesh.json",
     "mesh.pretty.json",
-    "nested.json",
+    //"nested.json",
     "svg_menu.json",
     "truenull.json",
     "twitter.json",
@@ -28,15 +28,25 @@ immutable int max_string_length = files.map!"a.length".reduce!max + folder.lengt
 void run_benchmark(string fileName)
 {
     auto text =  cast(string) fileName.read;
-
+    text ~= '\0';
     const size_t N = 1000;
     Duration minTime = Duration.max, avgTime;
+
+    import std.experimental.allocator.mallocator : Mallocator;
+
     foreach (size_t i; 0 .. N)
     {
         auto sw = StopWatch();
         sw.start;
-        auto json = text.parseJson(text.length * 2);
+        import std.typecons: No, Yes;
+        auto json = text.parseJson!(
+            Yes.includingNewLine,
+            Yes.spaces,
+            No.assumeValid,
+            Yes.zeroTerminated,
+        )(Mallocator.instance);
         sw.stop;
+        Mallocator.instance.deallocate(json.data.ptr[0 .. text.length * 6]);
         auto time = sw.peek.to!Duration;
         avgTime += time;
         minTime = min(minTime, time);
